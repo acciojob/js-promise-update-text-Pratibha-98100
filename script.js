8c04f9afecf2ac7b36c87b65eae10a67b350183a
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-08 09:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function delayedGreeting(){
+	return new Promise((resolve) => {
+		setTimeout(() =>{
+		  resolve('Hello, world!')
+		} , 1000)
+	})
+}
+
+const box = document.getElementById("output");
+
+let x = delayedGreeting();
+x.then((data) => {
+	box.innerText =  data 
+})
+// +'Hello, world!'
